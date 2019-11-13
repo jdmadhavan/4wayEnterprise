f25b55e9d968fd9231e351028733fc493a6e4705
madhavan_r <madhavan_r@CHD122908.photonint.com>
2019-11-13 14:33:01 +0530

    committing for Onfield research

--- MyWay/src/main/java/way/way/MyFirstStep.java
+++ MyWay/src/main/java/way/way/MyFirstStep.java
@@ -5,6 +5,7 @@ public class MyFirstStep {
 	public static void main(String[] args) {
 		System.out.println("Planing to start small Business");
 		System.out.println("Select Business Type");
+		System.out.println("Doing Onfield Research");
 
 	}
 
