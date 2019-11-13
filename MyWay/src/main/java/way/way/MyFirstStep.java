4b4c9059b9dbba6a65a49fd8952c908add8a19b3
madhavan_r <madhavan_r@CHD122908.photonint.com>
2019-11-13 13:50:44 +0530

    Commiting to branch

--- MyWay/src/main/java/way/way/MyFirstStep.java
+++ MyWay/src/main/java/way/way/MyFirstStep.java
@@ -4,6 +4,7 @@ public class MyFirstStep {
 
 	public static void main(String[] args) {
 		System.out.println("Planing to start small Business");
+		System.out.println("Select Business Type");
 
 	}
 
