6dc8ebf24e627bb28d82974323c3601867e3111c
madhavan_r <madhavan_r@CHD122908.photonint.com>
2019-11-13 16:58:03 +0530

    Commiting for OLD

--- MyWay/src/main/java/way/way/MyThirdStep.java
+++ MyWay/src/main/java/way/way/MyThirdStep.java
@@ -5,6 +5,7 @@ public class MyThirdStep {
 	public static void main(String[] args) {
 		System.out.println("Finding Investor");
         System.out.println("Ads");
+        System.out.println("AdsOld");
 	}
 
 }
