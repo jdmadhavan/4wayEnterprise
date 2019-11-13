adda72cc07634cfd54be22991168b9c7e0998ff3
madhavan_r <madhavan_r@CHD122908.photonint.com>
2019-11-13 15:05:28 +0530

    Push the local branch to Cloud Branch

--- MyWay/src/main/java/way/way/MyFirstStep.java
+++ MyWay/src/main/java/way/way/MyFirstStep.java
@@ -6,6 +6,7 @@ public class MyFirstStep {
 		System.out.println("Planing to start small Business");
 		System.out.println("Select Business Type");
 		System.out.println("Doing Onfield Research");
+		
 
 	}
 
